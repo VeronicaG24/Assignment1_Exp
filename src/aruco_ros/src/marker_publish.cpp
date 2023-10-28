394af2e556df21b84111cb2488159ba03d2e69c6
alemuraa <ale-mura2000@gmail.com>
2023-10-28 15:30:33 +0000

    ok

--- src/aruco_ros/src/marker_publish.cpp
+++ src/aruco_ros/src/marker_publish.cpp
@@ -23,7 +23,7 @@
 
  The views and conclusions contained in the software and documentation are those of the
  authors and should not be interpreted as representing official policies, either expressed
- or implied, of Rafael Muñoz Salinas.
+ or implied, of Rafael Muñoz Salinas. ok
  ********************************/
 /**
  * @file marker_publish.cpp
